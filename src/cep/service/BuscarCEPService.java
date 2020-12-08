1760afa1c3c3b50b1fcd122d120fe05f28767bb1
yurisperandio <yuri.sperandio@gmail.com>
2020-12-07 22:15:39 -0300

    Update BuscarCEPService.java
    JavaDoc

--- src/cep/service/BuscarCEPService.java
+++ src/cep/service/BuscarCEPService.java
@@ -9,12 +9,24 @@ import com.google.gson.GsonBuilder;
 
 import cep.BuscarCEP;
 
+/** Classe que realiza a chamada da WebService http://viacep.com.br/ws/ 
+ * @author Yuri
+ * @version 1.0 07 Dez 2020
+ *  */
 public class BuscarCEPService {
+	/**Propriedade endereço do WebService*/
 	static String urlWs = "http://viacep.com.br/ws/";
+	/**Propriedade de conexão obtida com o WebService*/
 	static int sucesso = 200;
 
+	
+	/** Método que realiza a chamada da WebService http://viacep.com.br/ws/ 
+	 * @author Yuri
+	 * @version 1.0 07 Dez 2020
+	 * @param cep
+	 *  */
 	public static BuscarCEP buscaEnderecoPeloCep(String cep) throws Exception {
-
+		/**Propriedade url do WebService*/
 		String chamada = urlWs + cep + "/json";
 
 		try {
